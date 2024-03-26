88b5c2ad5b9eec5debc9a7c7269d9c4a0d891c7d
jvariara <jvariara@gmail.com>
2024-03-26 15:56:32 -0400

    removed trpc/server files

--- src/trpc/trpc.ts
+++
@@ -1,8 +0,0 @@
-import { initTRPC } from "@trpc/server";
-
-// serve the purpose of giving router that lets us define api endpoints on backend
-const t = initTRPC.context().create();
-
-export const router = t.router;
-// so anyone can call these endpoints
-export const publicProcedure = t.procedure;
